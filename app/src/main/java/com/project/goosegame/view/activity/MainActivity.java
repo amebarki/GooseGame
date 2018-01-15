9b316a592efa2ba2d845cdeeca0feb97cf428491
amebarki <adam.mebarki@etu.univ-lyon1.fr>
2018-01-15 13:53:47 +0100

    First commit

---
+++ app/src/main/java/com/project/goosegame/view/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.project.goosegame.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.project.goosegame.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
