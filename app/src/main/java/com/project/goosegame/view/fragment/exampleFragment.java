cec7899a1e55f46200ceaac7b77140949b816ba6
amebarki <adam.mebarki@etu.univ-lyon1.fr>
2018-01-15 14:12:00 +0100

    Add examples

---
+++ app/src/main/java/com/project/goosegame/view/fragment/exampleFragment.java
@@ -0,0 +1,8 @@
+package com.project.goosegame.view.fragment;
+
+/**
+ * Created by Adam on 15/01/2018.
+ */
+
+public class exampleFragment {
+}
---
+++ app/src/main/java/com/project/goosegame/viewModel/exampleViewModel.java
@@ -0,0 +1,8 @@
+package com.project.goosegame.viewModel;
+
+/**
+ * Created by Adam on 15/01/2018.
+ */
+
+public class exampleViewModel {
+}
